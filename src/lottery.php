b3a114db7123220d196fb20ddfbaf54f802ab134
mamura <mamura@leoa.com.br>
2021-11-30 18:32:57 -0300

    question 02

--- src/lottery.php
+++ src/lottery.php
@@ -6,4 +6,44 @@ class Lottery
     private $resultado;
     private $totalJogos;
     private $jogos;
+
+    public function getQuantidadeDezenas()
+    {
+        return $this->quantidadeDezenas;
+    }
+
+    public function setQuantidadeDezenas($quantidadeDezenas)
+    {
+        $this->quantidadeDezenas = $quantidadeDezenas;
+    }
+
+    public function getResultado()
+    {
+        return $this->resultado;
+    }
+
+    public function setResultado($resultado)
+    {
+        $this->resultado = $resultado;
+    }
+
+    public function getTotalJogos()
+    {
+        return $this->totalJogos;
+    }
+
+    public function setTotalJogos($totalJogos)
+    {
+        $this->totalJogos = $totalJogos;
+    }
+
+    public function getJogos()
+    {
+        return $this->jogos;
+    }
+
+    public function setJogos($jogos)
+    {
+        $this->jogos = $jogos;
+    }
 }
